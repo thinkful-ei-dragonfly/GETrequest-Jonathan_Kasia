ee9ffa5704567f9ca3f6c5aad4b247adfca914a5
Kasia <kasia@miroartdesign.com>
2019-05-07 12:32:49 -0400

    fetch function added

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 'use strict';
-console.log('hi')
+
+
+
+
+const STATE = {
+    image: null,
+    caption: '',
+};
+
+
+
+function getDogImage(){
+    //return obj with data image url that we save in the store
+    return fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson));
+    
+}
+
+
+
+
+
+
+
+
+
+
+function render() {
+    
+}
+
+
+
+
