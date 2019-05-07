7cfb8b612c14c7ec5dbfe49dca5695bf2fb759d7
Jonathan Moore <jmjonemoore@gmail.com>
2019-05-07 14:13:00 -0500

    Dogs on the page

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 'use strict';
-
+/* global $ */
 
 
 
 const STATE = {
-    image: null,
-    caption: '',
+  dogs: [],
 };
 
-
-
-function getDogImage(){
-    //return obj with data image url that we save in the store
-    return fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
-    
+function addDogsToState(dogs){
+  STATE.dogs = dogs;
 }
 
-
-
-
-
-
-
-
-
-
-function render() {
-    
+function render(){
+  const html = STATE.dogs.map(dogImg => {
+    return `<li> 
+       <img src= ${dogImg}></li>`; 
+  });
+  return $('ul').html(html.join(''));
+}
+function getDogImage(n){
+  //return obj with data image url that we save in the store
+  //If n is greater than 50, return some error.
+  return fetch(`https://dog.ceo/api/breeds/image/random/${n}`)
+    .then(response => response.json());
 }
 
+function handleClick(){
+  $('#random-dog-img-form').submit(e => {
+    e.preventDefault();
+    const inputVar = $('#for-dog-image-entry').val();
+    getDogImage(inputVar)
+      .then(response => {
+        addDogsToState(response.message);
+        render();
+      });
+  });
+}   
 
-
-
+$(() =>{
+  handleClick();
+});
